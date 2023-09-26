1a58c26ed6fc0419b3b381efd8a71d1a2a0630f1
Özgür Taylan Akdağ <ozgur.akdag@acibadem.com>
2023-09-26 11:48:23 +0300

    Add project files.

---
+++ Character/Character.Api/Controllers/CharacterController.cs
@@ -0,0 +1,36 @@
+﻿using Character.Api.Models;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Character.Api.Controllers
+{
+	[ApiController]
+	[Route("api/[controller]")]
+	public class CharacterController : ControllerBase
+	{
+		private static List<Models.Character> characters = new List<Models.Character>
+		{
+			new Models.Character(),
+			new Models.Character { Id = 1, Name = "Sam" }
+		};
+
+		[HttpGet("GetAll")]
+		public ActionResult<List<Models.Character>> Get()
+		{
+			return Ok(characters);
+		}
+
+		[HttpGet("{id}")]
+		public ActionResult<Models.Character> GetSingle(int id)
+		{
+			return Ok(characters.FirstOrDefault(c => c.Id == id));
+		}
+
+		[HttpPost]
+		public ActionResult<List<Models.Character>> AddCharacter(Models.Character newCharacter)
+		{
+			characters.Add(newCharacter);
+
+			return Ok(characters);
+		}
+	}
+}
