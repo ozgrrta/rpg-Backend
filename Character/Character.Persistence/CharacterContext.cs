d1f1234c5d33dffde840d2199ccbf0f3b5451be1
Özgür Taylan Akdağ <oz.grrta@gmail.com>
2023-10-04 15:09:39 +0300

    persistence layer created in Characters necessary nuget packages added to use sql server with EF CharacterContext created

---
+++ Character/Character.Persistence/CharacterContext.cs
@@ -0,0 +1,15 @@
+﻿using CharacterDomain.Models;
+using Microsoft.EntityFrameworkCore;
+
+namespace CharacterPersistence
+{
+	public class CharacterContext : DbContext
+	{
+		public CharacterContext(DbContextOptions<CharacterContext> options) : base(options)
+		{
+
+		}
+
+		public DbSet<Character> Characters => Set<Character>();
+	}
+}
